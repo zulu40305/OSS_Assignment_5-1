6ffc729b7c301be9067971ec857ecdf15e284a23
zulu40305 <zulu40305@gmail.com>
2024-11-21 16:39:27 +0900

    assignments added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ShowList from "./components/pages/showList";
+import ShowList from "./pages/showList";
 
 const myComponent = <ShowList />;
 
